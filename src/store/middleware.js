a3c10e9626555afa098b0228e2b3838a8350e282
Bogdan Duduman <bogdan.duduman@cgm.com>
2021-10-11 17:47:44 +0300

    Add middleware setup: compose logger and thunk

---
+++ src/store/middleware.js
@@ -0,0 +1,13 @@
+import { applyMiddleware, compose } from "redux";
+import thunk from 'redux-thunk'
+import createLogger from 'redux-logger'
+
+
+const middlewareList = [];
+
+middlewareList.push(thunk);
+middlewareList.push(createLogger());
+
+const middlewares = compose(applyMiddleware(...middlewareList))
+
+export default middlewares;
