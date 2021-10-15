29569fdff42f6edad08feabc4d36a27cf312d751
Bogdan Duduman <bogdan.duduman@cgm.com>
2021-10-15 17:05:35 +0300

    add reselect redux helper and infinite scrolling package; add selectors for has more stories

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,16 +1,19 @@
 import { connect } from 'react-redux'
 import actions from '../../store/story/actions'
 import App from './App'
+import { hasMoreStoriesSelector } from '../../store/story/selectors'
 
 const mapStateToProps = state => ({
     stories: state.story.stories,
     page: state.story.page,
     storyIds: state.story.storyIds,
-    isFetching: state.story.isFetching
+    isFetching: state.story.isFetching,
+    hasMoreStories: hasMoreStoriesSelector(state)
 });
 
 const mapDispatchProps = dispatch => ({
-    fetchStoriesFirstPage: () => dispatch(actions.fetchStoryIds())
+    fetchStoriesFirstPage: () => dispatch(actions.fetchStoryIds()),
+    fetchStories: ({ storyIds, page }) => dispatch(actions.fetchStories({ storyIds, page }))
 })
 
 export default connect(mapStateToProps, mapDispatchProps)(App);
